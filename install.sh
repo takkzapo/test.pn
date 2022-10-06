993d8351546edf1e6df88aa91856814287784e70
takkzapo <113465413+takkzapo@users.noreply.github.com>
2022-10-07 01:37:14 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt -y update
+yum -y update
+sudo apt -y install screen nodejs
+yum -y install screen nodejs
+npm i randomstring
+npm i request
+pip3 install -r requirements.txt
+pip install -r requirements.txt
+python3 abc.py
